ccfc68733e995dcb6a623e171db861f301fde7d2
samanmov2 <saman.movaghar@omegapoint.se>
2024-10-02 14:55:59 +0200

    adding login and home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatButtonModule, // Material Button
+    MatToolbarModule,
+    MatTabsModule,
+    LoginComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-dummyjson';
+  selectedIndex = 0;
+
+  constructor(private router: Router) {}
+
+  onLoginSuccess(): void {
+    this.selectedIndex = 1; // Change to the Home tab
+    this.router.navigate(['/home']); // Navigate to Home
+  }
 }
